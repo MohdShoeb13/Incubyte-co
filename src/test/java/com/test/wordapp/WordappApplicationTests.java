b74541ba82e15aed0c6b49df1e496075aae74101
Max Dev <shoebmoh13@gmail.com>
2022-04-02 16:02:15 +0530

    adding spring boot starter backend

---
+++ src/test/java/com/test/wordapp/WordappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.wordapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
